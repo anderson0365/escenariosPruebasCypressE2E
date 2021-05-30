ab4ab9deb6870a95393f35e64d3117e3c596a48e
Anderson Luis Cáceres Padilla <al.caceres@uniandes.edu.co>
2021-05-30 12:58:02 -0500

    Se agregan delays en esenarios de taga para que la prueba no falle

--- cypress/page-objects/tags-page.js
+++ cypress/page-objects/tags-page.js
@@ -27,25 +27,32 @@ export class GhostTester{
 
     selectTagsFromManageMenu(){
         cy.get('a[href="#/tags/"]').first().click({force: true});
+        cy.wait(1000);
     }
 
     selectPostsfromManageMenu(){
         cy.get('a[href="#/posts/"]').first().click({force: true});
+        cy.wait(1000);
     }
 
     selectPagesFromManageMenu(){
         cy.get('a[href="#/pages/"]').first().click({force: true});
+        cy.wait(1000);
     }
 
     createAPost(postTitle){
         cy.get('a[href="#/editor/post/"]').first().click({force: true});
+        cy.wait(1000);
         cy.get('textarea[placeholder="Post Title"]').focus().type(postTitle);
+        this.takeScreenshot();
         cy.get('div[data-placeholder="Begin writing your post..."]').focus();
     }
 
     createAPage(pageTitle){
         cy.get('a[href="#/editor/page/"]').first().click({force: true});
+        cy.wait(1000);
         cy.get('textarea[placeholder="Page Title"]').focus().type(pageTitle);
+        this.takeScreenshot();
         cy.get('div[data-placeholder="Begin writing your page..."]').focus();
     }
 
@@ -65,17 +72,20 @@ export class GhostTester{
     associatedTagToPost(tagName){
         cy.get('button.post-settings').click();
         cy.get('#tag-input').focus().type(tagName + '{enter}');
+        this.takeScreenshot();
         cy.get('button.close').click();
     }
 
     detachLastTagFromPost(){
         cy.get('button.post-settings').click();
         cy.get('#tag-input').focus().type('{backspace}');
+        this.takeScreenshot();
         cy.get('button.close').click();
     }
 
     checkTagDontHavePostsRelated(tagSlug){
         cy.get(`a.gh-tag-list-posts-count[href="#/tags/${tagSlug}/"] > span`).should('have.text','0 posts');
+        this.takeScreenshot();
     }
 
 
@@ -84,6 +94,7 @@ export class GhostTester{
         cy.get('#tag-name').focus().type(tagName);
         cy.get('#tag-slug').focus().clear().type(tagSlug.toLowerCase());
         cy.get('#tag-description').focus().type(tagDescription);
+        this.takeScreenshot();
         cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').click();
     }
 
@@ -91,20 +102,29 @@ export class GhostTester{
         cy.get(`a[href="#/tags/${tagSlug.toLowerCase()}/"]`).first().click({force: true});
         cy.get('button[class="gh-btn gh-btn-red gh-btn-icon mb15"]').click()
         cy.get('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').first().click()
+        this.takeScreenshot();
     }
 
     validateTagAssociation(tagName, postTitle){
         cy.get(`a[title="List posts tagged with '${tagName}'"]`).click({force: true});
         cy.contains(postTitle);
+        this.takeScreenshot();
     }
 
     validateTagAssociationWithPage(tagName){
         cy.get(`a[title="List posts tagged with '${tagName}'"] > span`).should('have.text','1 post');
+        this.takeScreenshot();
     }
 
     removePost(){
         cy.get('button.post-settings').click();
         cy.get('button.settings-menu-delete-button').click();
         cy.get('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').first().click();
+        this.takeScreenshot();
+    }
+
+    takeScreenshot(){
+        //this.stepCounter++;
+        //cy.screenshot(`${this.scenario}_${this.stepCounter}`);
     }
 }
